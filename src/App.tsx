23a844c5c18e6a633ab629db8f84a2b96b2b697a
nyrellcl <nyrell.leonor2000@gmail.com>
2023-05-19 15:33:02 -0700

    add colors and format nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import * as React from "react";
 import { useState } from "react";
+import { Switch } from "@mui/material";
 import { fetchDefinitions } from "./api/api";
 import logo from "./assets/images/logo.svg";
-import "./App.css";
+import searchIcon from "./assets/images/icon-search.svg";
 
 interface Definition {
   word: string;
+  phonetics: Array<{
+    text: string;
+    audio: string;
+  }>;
   meanings: Array<{
     partOfSpeech: string;
-    definitions: Array<{ definition: string }>;
+    definitions: Array<{ definition: string; synonyms: string }>;
   }>;
 }
 
@@ -47,80 +52,127 @@ function App() {
   };
 
   return (
-    <main>
+    <main style={fontStyles}>
       <header>
-        <nav>
+        <nav className="nav-bar">
           <div className="logo">
             <a href="/">
               <img src={logo} alt="" />
             </a>
           </div>
 
-          <select
-            value={font}
-            onChange={handleFontChange}
-            name="font-select"
-            id="FONTS"
-            className="font-selection"
-          >
-            <option value="serif">Serif</option>
-            <option value="monospace">Mono</option>
-            <option value="sans-serif">Sans Serif</option>
-          </select>
-        </nav>
-
-        {isLight ? (
-          <div className="switch-container">
-            <input
-              type="radio"
-              name="switch"
-              id="switch"
-              onClick={handleModeSwitch}
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="22"
-              viewBox="0 0 22 22"
+          <fieldset className="state-selections">
+            <label htmlFor="font-select">{font}</label>
+            <select
+              value={font}
+              onChange={handleFontChange}
+              name="font-select"
+              id="FONTS"
+              className="font-selection"
+              placeholder=""
             >
-              <path
-                fill="none"
-                stroke="#838383"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
-              />
-            </svg>
+              <option value="Serif">Serif</option>
+              <option value="Monospace">Mono</option>
+              <option value="Sans-serif">Sans Serif</option>
+            </select>
+            {isLight ? (
+              <div className="switch-container">
+                <Switch onClick={handleModeSwitch} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 22 22"
+                >
+                  <path
+                    fill="none"
+                    stroke="#838383"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
+                  />
+                </svg>
+              </div>
+            ) : (
+              <div className="switch-container">
+                <Switch onClick={handleModeSwitch} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="22"
+                  height="22"
+                  viewBox="0 0 22 22"
+                >
+                  <path
+                    fill="none"
+                    stroke="#a445ed"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="1.5"
+                    d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
+                  />
+                </svg>
+              </div>
+            )}
+          </fieldset>
+        </nav>
+      </header>
+      <section className="definitions-container">
+        <form onSubmit={handleWordSearch} method="POST" className="form-container">
+          <input
+            name="search-word"
+            id="search-word"
+            type="text"
+            value={word}
+            onChange={(e) => setWord(e.target.value)}
+            placeholder="Search for any word..."
+          />
+
+          <button type="submit" className="search-btn">
+            <img src={searchIcon} alt="search-icon" />
+          </button>
+        </form>
+
+        {definitions.length > 0 ? (
+          <div>
+            <article className="word-pronounciation">
+              <h2 className="word-pronounciation__term">
+                {definitions[0].word}
+              </h2>
+              <span className="word-pronounciation__phonetics">
+                {definitions[0].phonetics[0].text}
+              </span>
+            </article>
+
+            {definitions.map((definition, index) => (
+              <>
+                <article className="noun-container" key={index}>
+                  <h3>{definition.meanings[0].partOfSpeech}</h3>
+                  <small>Meaning</small>
+                  <ul>
+                    {definition.meanings[0].definitions.map((item, index) => (
+                      <li key={index}>{item.definition}</li>
+                    ))}
+                  </ul>
+                </article>
+                <article
+                  className="verb-container"
+                  key={definition.meanings[1].partOfSpeech}
+                >
+                  <h3>{definition.meanings[1].partOfSpeech}</h3>
+                  <small>Meaning</small>
+                  <ul>
+                    {definition.meanings[1].definitions.map((items, index) => (
+                      <li key={index}>{items.definition}</li>
+                    ))}
+                  </ul>
+                </article>
+              </>
+            ))}
           </div>
         ) : (
-          <div className="switch-container">
-            <input
-              type="radio"
-              name="switch"
-              id="switch"
-              onClick={handleModeSwitch}
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="22"
-              height="22"
-              viewBox="0 0 22 22"
-            >
-              <path
-                fill="none"
-                stroke="#a445ed"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="1.5"
-                d="M1 10.449a10.544 10.544 0 0 0 19.993 4.686C11.544 15.135 6.858 10.448 6.858 1A10.545 10.545 0 0 0 1 10.449Z"
-              />
-            </svg>
-          </div>
+          <p>Enter a word to search.</p>
         )}
-      </header>
-      <section>
-        <form onSubmit={handleWordSearch}></form>
       </section>
     </main>
   );
